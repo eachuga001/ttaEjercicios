7c73aaa062d0609dd65d004597c682266d5c3eac
eachuga001 <eachuga001@gmail.com>
2018-01-15 11:41:21 +0100

    hecha la parte de grabacion de audio

--- Practica1/app/src/main/java/eus/ehu/tta/practica1/NuevoEjercicioActivity.java
+++ Practica1/app/src/main/java/eus/ehu/tta/practica1/NuevoEjercicioActivity.java
@@ -1,13 +1,89 @@
 package eus.ehu.tta.practica1;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.io.File;
+
+import eus.ehu.tta.practica1.model.FileOperations;
 
 public class NuevoEjercicioActivity extends AppCompatActivity {
 
+    private static final int READ_REQUEST_CODE = 0;
+    private static final int VIDEO_REQUEST_CODE = 1;
+    private static final int AUDIO_REQUEST_CODE = 2;
+    private static final int PICTURE_REQUEST_CODE = 3;
+    private static final int SAVE_REQUEST_CODE = 4;
+
+    private LinearLayout layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_ejercicio);
+        layout = (LinearLayout)findViewById(R.id.layoutNuevoEjercicioActivity);
+    }
+    //@Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        if(resultCode != Activity.RESULT_OK)
+            return;
+        switch (requestCode){
+            case READ_REQUEST_CODE:
+
+                break;
+            //case VIDEO_REQUEST_CODE:
+            case AUDIO_REQUEST_CODE:
+                Toast.makeText(this,R.string.successSaving,Toast.LENGTH_SHORT).show();
+
+                break;
+            case PICTURE_REQUEST_CODE:
+
+                break;
+            case SAVE_REQUEST_CODE:
+                //FileOperations f = new FileOperations();
+                /*if(f.isExternalStorageWritable()) {
+                    f.getAlbumStorageDir("AudioPractica1");
+                    Toast.makeText(this,R.string.successSaving,Toast.LENGTH_SHORT).show();
+                }else
+                    Toast.makeText(this,R.string.errorSave,Toast.LENGTH_SHORT).show();
+                */
+
+                break;
+        }
+    }
+
+    public void sendFile(View view, Uri uri){
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        startActivityForResult(intent,SAVE_REQUEST_CODE);
+    }
+
+    public void clickRecAudio(View view){
+        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_MICROPHONE))
+            Toast.makeText(this,R.string.noMicrophone,Toast.LENGTH_SHORT).show();
+        else{
+            Intent intent = new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
+            if(intent.resolveActivity(getPackageManager())!=null)
+                startActivityForResult(intent, AUDIO_REQUEST_CODE);
+            else
+                Toast.makeText(this,R.string.noApp,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void clickRecVideo(View view){
+
+    }
+
+    public void clickUploadFile(View view){
+
     }
 }
---
+++ Practica1/app/src/main/java/eus/ehu/tta/practica1/model/FileOperations.java
@@ -0,0 +1,41 @@
+package eus.ehu.tta.practica1.model;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+
+/**
+ * Created by edwin on 15/01/18.
+ */
+
+public class FileOperations {
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    public File getAlbumStorageDir(String albumName) {
+        // Get the directory for the user's public pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_MUSIC), albumName);
+        if (!file.mkdirs()) {
+            System.out.println("DIRECTORY NO CREATED");
+        }
+        return file;
+    }
+}
